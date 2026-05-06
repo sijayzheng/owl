c75bdc8bf00e506cff871d7bf132d210f8393668
sijay <sijayzheng@gmail.com>
2026-05-06 23:41:08 +0800

    更新前端

--- ui/src/api/system/sysUserApi.ts
+++ ui/src/api/system/sysUserApi.ts
@@ -1,38 +1,38 @@
-export const sysUserApi = {
+export const SysUserApi = {
   // 分页查询系统用户列表
   page(data: SysUserQuery) {
-    return request.page<Result<Array<SysUser>>>('/system/sys_user/page')
+    return request.page<Result<Array<SysUser>>>("/system/sys-user/page", data)
   },
   // 查询系统用户列表
   list(data: SysUserQuery) {
-    return request.get<Array<SysUser>>('/system/sys_user/list')
+    return request.get<Array<SysUser>>("/system/sys-user/list", data)
   },
   // 根据ID查询系统用户详情
   getById(id: number) {
-    return request.get<SysUser>('/system/sys_user/{id}')
+    return request.get<SysUser>("/system/sys-user/{id}")
   },
   // 新增系统用户
   add(data: SysUserForm) {
-    return request.post<boolean>('/system/sys_user/add')
+    return request.post<boolean>("/system/sys-user/add", data)
   },
   // 修改系统用户
   update(data: SysUserForm) {
-    return request.post<boolean>('/system/sys_user/update')
+    return request.post<boolean>("/system/sys-user/update", data)
   },
   // 删除系统用户
   remove(ids: number[]) {
-    return request.post<boolean>('/system/sys_user/remove', [...ids])
+    return request.post<boolean>("/system/sys-user/remove", ids)
   },
   // 下载系统用户导入模板
-  // template() {
-  //   return request.get<>('/system/sys_user/template')
-  // },
+  //template() {
+  //  return request.get<>("/system/sys-user/template")
+  //},
   // 导入系统用户数据
-  // importData() {
-  //   return request.post<>('/system/sys_user/import')
-  // },
+  //importData() {
+  //  return request.post<>("/system/sys-user/import")
+  //},
   // 导出系统用户数据
-  // exportData(data: SysUserQuery) {
-  //   return request.get<>('/system/sys_user/export')
-  // },
+  //exportData(data: SysUserQuery) {
+  //  return request.get<>("/system/sys-user/export", data)
+  //},
 }
--- ui/src/views/index.vue
+++ ui/src/views/index.vue
@@ -1,10 +1,12 @@
 <template>
-  <main>
-    <HelloWorld msg="You did it!"/>
-    <TheWelcome/>
-  </main>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
+      this is the index page
+    </h3>
+  </div>
 </template>
 <script setup lang="ts">
-import TheWelcome from '@/components/TheWelcome.vue'
-import HelloWorld from '@/components/HelloWorld.vue'
+
+const msg = ref('Hello')
 </script>
